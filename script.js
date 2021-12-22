177f3157cd6950460844a4d426bc8cda49a1fb7e
Eugenie Khayat <eugenie.khayat@aduneo.com>
2021-12-22 18:20:02 +0100

    d

---
+++ script.js
@@ -0,0 +1,29 @@
+/* DEBUT MENU HAMBURGER */
+
+let hamburger = document.getElementById("menu-content");
+
+function showHamburger(){
+    if (hamburger.style.display === "none"){
+        hamburger.style.display = "block";
+    } else {
+        hamburger.style.display = "none";
+    }
+}
+
+
+
+
+
+
+
+// function showHamburger() {
+//     if (hamburger.style.display === "none") {
+//         hamburger.style.display === "block";
+//     } else {
+//         hamburger.style.display === "none";
+//     }
+// }
+
+    
+
+/* FIN MENU HAMBURGER */
