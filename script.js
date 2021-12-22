265bb1098bc314bbff1c92cbbea5bab41217a19c
Eugenie Khayat <eugenie.khayat@aduneo.com>
2021-12-22 18:26:45 +0100

    menu hamburger

--- script.js
+++ script.js
@@ -3,10 +3,10 @@
 let hamburger = document.getElementById("menu-content");
 
 function showHamburger(){
-    if (hamburger.style.display === "none"){
-        hamburger.style.display = "block";
-    } else {
+    if (hamburger.style.display === "block"){
         hamburger.style.display = "none";
+    } else {
+        hamburger.style.display = "block";
     }
 }
 
